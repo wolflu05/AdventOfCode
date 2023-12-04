ef901080e79c77884a324adaeb6acc6186ab730b
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-12-04 07:42:34 +0100

    [2023/04] Added py solution

---
+++ 2023/04_Scratchcards.py
@@ -0,0 +1,35 @@
+import sys, re, functools
+
+inp = []
+with open(sys.argv[1]) as f:
+    inp = [l.strip() for l in f.readlines()]
+
+p1 = 0
+cards = {}
+
+for l in inp:
+    c, a, b = re.split(":|\|", l)
+    n = int(c.split()[1])
+    
+    a = set(int(h) for h in a.split())
+    b = set(int(h) for h in b.split())
+    w = len(b & a)
+
+    p1 += 2 ** (w - 1) if w else 0
+    cards[n] = w
+
+ll = len(cards.keys())
+
+@functools.cache
+def get_count(i):
+    r = cards[i]
+    s = 1
+    for h in range(r):
+        k = i + h + 1
+        if k <= ll:
+            s += get_count(k)
+
+    return s
+
+print(p1)
+print(sum(get_count(i) for i in cards.keys()))        
