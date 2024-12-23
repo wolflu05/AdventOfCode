59a3b512b52c5a6744aace02bb5b02adfedf5ef0
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2024-12-23 07:10:17 +0100

    [py/2024/23] add solution

---
+++ languages/py/2024/23_LANParty.py
@@ -0,0 +1,49 @@
+import sys, collections, re, itertools, functools, math
+from lib import *
+
+inp = get_input()
+
+G = collections.defaultdict(set)
+
+for l in inp:
+    a,b = l.split("-")
+
+    G[a].add(b)
+    G[b].add(a)
+
+def all_connected(nodes):
+    nodes = set(nodes)
+    for n in nodes:
+        for g in nodes - {n}:
+            if g not in G[n]:
+                return False
+            
+    return True
+
+L = len(next(iter(G.values())))
+
+p1 = None
+p2 = None
+
+for i in range(L, 1, -1):
+    F = set()
+
+    if p2 is not None and i != 2:
+        continue
+
+    for k,v in G.items():
+        for group in itertools.combinations(v, r=i):
+            if all_connected(group):
+                F.add(frozenset((k,*group)))
+
+    if i == 2:
+        p1 = 0
+        for group in F:
+            if any(x.startswith("t") for x in group):
+                p1 += 1
+
+    if len(F) == 1:
+        p2 = ",".join(sorted(F.pop()))
+
+print(p1)
+print(p2)
